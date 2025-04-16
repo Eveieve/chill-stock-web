8afd613a01795891e8bab880b67503f912a56115
dlwjdtjq001 <dlwjdtjq001@gmail.com>
2025-04-16 18:55:13 +0900

    chore/mybatis

--- src/main/java/com/chilluminati/chillstock/HelloController.java
+++ src/main/java/com/chilluminati/chillstock/HelloController.java
@@ -1,11 +1,14 @@
 package com.chilluminati.chillstock;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+
 
     @GetMapping("/hellotymeleaf")
     public String hello(Model model) {
--- src/main/java/com/chilluminati/chillstock/config/WebAppInitializer.java
+++ src/main/java/com/chilluminati/chillstock/config/WebAppInitializer.java
@@ -8,7 +8,8 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     protected Class<?>[] getRootConfigClasses() {
         return new Class[] {
                 ThymeleafConfig.class,
-                HikariCPConfig.class};// DB 설정이 따로 있다면 여기에 추가 기타 컨픽도 추가
+                HikariCPConfig.class,
+                MybatisConfig.class};// DB 설정이 따로 있다면 여기에 추가 기타 컨픽도 추가
     }
 
     @Override

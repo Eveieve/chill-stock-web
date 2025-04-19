9e40fbfb56d8b3cbf4ddc04e68213e89210cd660
Eveieve <jhkdev1@gmail.com>
2025-04-19 14:43:43 +0900

    refactor: BizVO 의 필드명 테이블 컬럼명과 똑같도록 수정

--- src/main/java/com/chilluminati/chillstock/nonuser/vo/BizVO.java
+++ src/main/java/com/chilluminati/chillstock/nonuser/vo/BizVO.java
@@ -14,13 +14,13 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BizVO {
 
-    private Integer bizId;                 // 사업체 ID (PK)
+    private Integer businessId;                 // 사업체 ID (PK)
 
-    private String bizRegistNum;           // 사업자등록번호 (UNIQUE, NOT NULL)
+    private String businessRegistNum;           // 사업자등록번호 (UNIQUE, NOT NULL)
 
-    private String bizName;                // 사업체 이름 (NOT NULL)
+    private String businessName;                // 사업체 이름 (NOT NULL)
 
-    private String bizAddress;             // 사업체 주소 (NOT NULL)
+    private String businessAddress;             // 사업체 주소 (NOT NULL)
 
     private Integer userId;                // 유저 ID (외래키, NOT NULL)
 

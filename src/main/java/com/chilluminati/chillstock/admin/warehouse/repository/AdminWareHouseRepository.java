c656c323b0a7252e6317bbf3330f78d0a3f0f7f1
dlwjdtjq001 <dlwjdtjq001@gmail.com>
2025-04-22 18:12:13 +0900

    feat/warehouse 1. 창고 리스트 기능 2. 창고 생성 주소 kakao api 연동

--- src/main/java/com/chilluminati/chillstock/admin/warehouse/repository/AdminWareHouseRepository.java
+++ src/main/java/com/chilluminati/chillstock/admin/warehouse/repository/AdminWareHouseRepository.java
@@ -3,7 +3,10 @@ package com.chilluminati.chillstock.admin.warehouse.repository;
 import com.chilluminati.chillstock.admin.warehouse.dto.AdminWarehouseDto;
 import com.chilluminati.chillstock.admin.warehouse.vo.AdminWarehouseVo;
 
+import java.util.List;
+
 public interface AdminWareHouseRepository {
     void createWarehouse(AdminWarehouseVo adminWarehouseVo);
     AdminWarehouseVo adminGetWarehouseById(Integer id);
+    List<AdminWarehouseVo> adminGetAllWarehouses();
 }

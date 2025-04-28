019c1903647482c3e7d192071177a3e9e723c85e
dlwjdtjq001 <dlwjdtjq001@gmail.com>
2025-04-28 09:25:17 +0900

    dashboard

--- src/main/java/com/chilluminati/chillstock/MainController.java
+++ src/main/java/com/chilluminati/chillstock/MainController.java
@@ -1,12 +1,19 @@
 package com.chilluminati.chillstock;
 
 
+import com.chilluminati.chillstock.admin.dashboard.DashBoardRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final DashBoardRepository dashBoardRepository;
+
     @GetMapping("/")
     public String helloMain() {
         return "index";
@@ -15,6 +22,14 @@ public class MainController {
     @GetMapping("/admin/home")
     public String hello(Model model) {
         model.addAttribute("message", "Hello, ChillStock!");
+        Integer inboundCount = dashBoardRepository.countInbounds();
+        model.addAttribute("inboundCount", inboundCount);
+        Integer outboundCount = dashBoardRepository.countOutbounds();
+        model.addAttribute("outboundCount", outboundCount);
+        Integer inboundTodayCount = dashBoardRepository.countTodayInboundRequests();
+        model.addAttribute("inboundTodayCount", inboundTodayCount);
+        Integer outboundTodayCount = dashBoardRepository.countTodayOutboundRequests();
+        model.addAttribute("outboundTodayCount", outboundTodayCount);
         return "admin/dashboard"; // → /WEB-INF/views/hello.html 로 매핑
     }
 

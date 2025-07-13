b02efc98abfadfd24e619918a3c7423c5c4775f1
nanhee <jhkdev1@gmail.com>
2025-07-13 19:32:14 +0900

    build : fix build issue - redis

--- src/main/java/com/chilluminati/chillstock/config/WebMvcConfig.java
+++ src/main/java/com/chilluminati/chillstock/config/WebMvcConfig.java
@@ -4,17 +4,26 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.chilluminati.chillstock")
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    //리소스 핸들러
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
-                .addResourceLocations("/static/");
+                .addResourceLocations("classpath:/static/", "file:src/main/webapp/static/");
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".html");
+        registry.viewResolver(resolver);
     }
 }

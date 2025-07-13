4d53f40256b696eae9a9022de84a9a0f293402e8
nanhee <jhkdev1@gmail.com>
2025-07-13 17:55:12 +0900

    build : fix error when './gradlew bootRun'

---
+++ src/main/java/com/chilluminati/chillstock/ChillStockApplication.java
@@ -0,0 +1,11 @@
+package com.chilluminati.chillstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillStockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChillStockApplication.class, args);
+    }
+} 
